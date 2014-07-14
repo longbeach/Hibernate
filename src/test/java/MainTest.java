f4f38f55961d30d807ce651400c764a637e8cc28
bilalsha <m.bilalshah1@gmail.com>
2014-07-14 04:08:54 -0700

    many to many relationship

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,12 +1,14 @@
 
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.hibernate.Session;
 
 import com.hibernate.persistance.HibernatePersistence;
-import com.hibernate.tutorial.Category;
-import com.hibernate.tutorial.Product;
+import com.hibernate.tutorial.Course;
+import com.hibernate.tutorial.Student;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -19,40 +21,39 @@ public class MainTest
     extends TestCase
 {
 	private static Session session;
-    private static Category category;
-    private static Product product;
+    private static Student student;
 
     
     @org.junit.Test
-    public void testCategory(){
-        String name = "Penalty";
-        category = new Category();
+    public Course testCourse(String name){
+        Course course = new Course();
         session = HibernatePersistence.getSessionFactory().openSession();
-        category.setName(name);
+        course.setName(name);
         session.beginTransaction();
-        
-        Integer categoryId = (Integer) session.save(category);
+        Integer courseId = (Integer) session.save(course);
         session.getTransaction().commit();
-        category = (Category) session.get(Category.class, categoryId);
-        
-        assertEquals("Penalty", category.getName());
+        course = (Course) session.get(Course.class, courseId);
+        System.out.println("Course Name is : "+course.getName());
+        return course;
     }
     
     @org.junit.Test
-    public void testProduct(){
-        product = new Product();
+    public void testStudent(){
+    	student = new Student();
         session = HibernatePersistence.getSessionFactory().openSession();
-        product.setName("COKE");
-        product.setCode("C002");
-        product.setPrice(new BigDecimal("18.00"));
-        product.setCategory(category);
+        student.setName("ALI");
+        Course mathCourse = testCourse("Math");
+        Course phyCourse = testCourse("PHY");
+        Set<Course> courses = new HashSet<Course>();
+        courses.add(mathCourse);
+        courses.add(phyCourse);
+        student.setCourses(courses);
         session.beginTransaction();
         
-        Integer productId =(Integer) session.save(product);
+        Integer studentId =(Integer) session.save(student);
         session.getTransaction().commit();
-        product = (Product) session.get(Product.class, productId);
-        
-        assertEquals("COKE",product.getName());
-        assertEquals(category.getId(), product.getCategory().getId());
+        student = (Student) session.get(Student.class, studentId);
+        System.out.println("Student name is : "+student.getName());
+        assertEquals("ALI",student.getName());
     }
 }
