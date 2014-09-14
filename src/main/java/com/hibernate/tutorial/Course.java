00e3e5d8c8aa9eb7bf50be3b242386afda24e854
bilalsha <m.bilalshah1@gmail.com>
2014-09-14 09:09:24 -0700

    Classes

---
+++ src/main/java/com/hibernate/tutorial/Course.java
@@ -0,0 +1,29 @@
+package com.hibernate.tutorial;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Course {
+
+	private Integer id;
+	private String name;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + "]";
+	}
+	
+}
---
+++ src/main/java/com/hibernate/tutorial/Student.java
@@ -0,0 +1,38 @@
+package com.hibernate.tutorial;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Student {
+
+	private Integer id;
+	private String name;
+	private Set<Course> courses = new HashSet<Course>();
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Set<Course> getCourses() {
+		return courses;
+	}
+	public void setCourses(Set<Course> courses) {
+		this.courses = courses;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", courses=" + courses
+				+ "]";
+	}
+	
+}
